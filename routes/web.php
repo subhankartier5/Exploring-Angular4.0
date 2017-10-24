3455c475d5d81a0889d471cfaf4de9b0547611bc
tier5 <work@tier5.us>
2017-10-24 22:05:30 +0530

    v0.1 Released

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,24 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
+    return response()->file( public_path() . '/index.html' );
+});
+Route::get('/{ng_route}', function () {
+    return response()->file( public_path() . '/index.html' );
+});
+Route::get('/{ng_route}/{ng_sub_route}', function () {
+    return response()->file( public_path() . '/index.html' );
+});
+Route::get('/{ng_route}/{ng_sub_route}/{ng_sub_sub_route}', function () {
+    return response()->file( public_path() . '/index.html' );
+});
+Route::get('/{ng_route}/{ng_sub_route}/{ng_sub_sub_route}/{ng_sub_sub_sub_route}', function () {
+    return response()->file( public_path() . '/index.html' );
+});
+Route::get('/{ng_route}/{ng_sub_route}/{ng_sub_sub_route}/{ng_sub_sub_sub_route}/{ng_sub_sub_sub_sub_route}', function () {
+    return response()->file( public_path() . '/index.html' );
+});
+Route::get('/{ng_route}/{ng_sub_route}/{ng_sub_sub_route}/{ng_sub_sub_sub_route}/{ng_sub_sub_sub_sub_route/{ng_sub_sub_sub_sub_sub_route}', function () {
+    return response()->file( public_path() . '/index.html' );
 });
